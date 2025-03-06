1333e7d573c3b2355aea19c1e8686bc63dae013e
ktalovmorx <jomorales.ventura@gmail.com>
2025-03-06 00:12:53 -0400

    Agregar vistas de datos de pacientes

--- app.py
+++ app.py
@@ -183,9 +183,11 @@ def login():
         # -- Redirigir al usuario a la página que intentaba acceder
         next_page = request.args.get('next')
 
-        if user.role == RoleEnum.paciente:
+        print(user.role.value, RoleEnum.paciente.value, RoleEnum.doctor.value)
+        
+        if user.role.value == RoleEnum.paciente.value:
             return redirect(next_page or url_for('get_pacient_page'))
-        elif user.role == RoleEnum.doctor:
+        elif user.role.value == RoleEnum.doctor.value:
             return redirect(next_page or url_for('get_pacient_list'))
         else:
             logout_user()
